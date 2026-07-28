d8486e3f69b91d34cca4aad7e1869079f07f7988
ls <liushuo.fe@gmail.com>
2026-07-28 15:53:33 +0800

    feat(JavaSEProjectMax): 多线程-抢红包游戏(50%)

---
+++ CJavaSEPorjectMax/src/main/java/org/example/d_thread/h_test/ThreadTest.java
@@ -0,0 +1,29 @@
+package org.example.d_thread.h_test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class ThreadTest {
+    public static void main(String[] args) {
+        // 目标：完成多线程综合小案例
+        // 红包雨游戏，某企业有100名员工，员工的工号依次是1，2，3，4，..到100。
+        // 现在公司举办了年会活动，活动中有一个红包雨环节，要求共计发出200个红包，其中小红包在[1-30] 元之间。
+        // 总占比为 80%，大红包[31-100]元，总占比为20%。
+        // 分析：100个员工实际上就是100个线程，来竞争200个红包。
+    }
+
+    // 准备这200个随机的红包返回，放到 List 集合中去返回。
+    public static List<Integer> getRedPacket(int count) {
+        Random r = new Random();
+        // 其中小红包在[1-30] 元之间，总占比为 80%。大红包[31-100]元，总占比为20%。
+        List<Integer> redPacket = new ArrayList<>();
+        for (int i = 0; i < count * 0.8; i++) {
+            redPacket.add(r.nextInt(30) + 1);
+        }
+        for (int i = 0; i < count * 0.2; i++) {
+            redPacket.add(r.nextInt(70) + 31);
+        }
+        return redPacket;
+    }
+}
