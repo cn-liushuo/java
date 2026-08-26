a773708abf56db57cfe94e7911493c273afe059d
ls <liushuo.fe@gmail.com>
2026-08-26 09:36:05 +0800

    feat(JavaSEProjectMax): Java 高级技术 —— 注解 —— 元注解

---
+++ CJavaSEPorjectMax/src/main/java/org/example/e_net/k_junit_reflect_annotation_proxy/c_annotation/B_AnnotationDemo.java
@@ -0,0 +1,23 @@
+package org.example.e_net.k_junit_reflect_annotation_proxy.c_annotation;
+
+// @MyTest1
+public class B_AnnotationDemo {
+
+    @MyTest1
+    private int age;
+
+    // @MyTest1
+    public B_AnnotationDemo() {
+
+    }
+
+    @MyTest1
+    public static void main(String[] args) {
+        // 目标：搞清楚元注解的作用。
+    }
+
+    @MyTest1
+    public void getAgeTest() {
+
+    }
+}
