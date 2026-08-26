91067fdfb89ed9eb2c70cf57775fb9fe99d0ba2e
ls <liushuo.fe@gmail.com>
2026-08-26 10:00:06 +0800

    feat(JavaSEProjectMax): Java 高级技术 —— 注解 —— 注解的解析

---
+++ CJavaSEPorjectMax/src/main/java/org/example/e_net/k_junit_reflect_annotation_proxy/c_annotation/Demo.java
@@ -0,0 +1,9 @@
+package org.example.e_net.k_junit_reflect_annotation_proxy.c_annotation;
+
+@MyTest2(value = "刘亦菲", address = {"北京", "上海"})
+public class Demo {
+    @MyTest2(value = "欧阳娜娜", address = {"湖南", "湖北"})
+    public void go() {
+
+    }
+}
