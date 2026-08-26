7f6e1dbe3e544e4aee2fabc442cae42daa4f10d9
ls <liushuo.fe@gmail.com>
2026-08-26 13:01:38 +0800

    feat(JavaSEProjectMax): Java 高级技术 —— 动态代理 —— 解决实际问题，掌握使用代理的好处

---
+++ CJavaSEPorjectMax/src/main/java/org/example/e_net/k_junit_reflect_annotation_proxy/e_proxy/UserService.java
@@ -0,0 +1,15 @@
+package org.example.e_net.k_junit_reflect_annotation_proxy.e_proxy;
+
+/**
+ * 用户业务接口
+ */
+public interface UserService {
+    // 登录功能
+    void login(String loginName, String passWord) throws Exception;
+
+    // 删除用户
+    void deleteUsers() throws Exception;
+
+    // 查询用户，返回数组的形式
+    String[] selectUsers() throws Exception;
+}
