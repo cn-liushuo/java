1d85067cf40577522d9aa5b4a48ea4e6f599c1a8
ls <liushuo.fe@gmail.com>
2026-09-07 15:36:50 +0800

    feat: maven 基础

---
+++ DJavaWeb/maven-project01/src/main/java/com/liushuo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.liushuo;
+
+import org.slf4j.Logger;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Logger log;
+        System.out.println("Hello Maven");
+    }
+}
---
+++ DJavaWeb/maven-project01/src/test/java/com/liushuo/test/DemoTest.java
@@ -0,0 +1,21 @@
+package com.liushuo.test;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+
+public class DemoTest {
+    @Test
+    public void test1() {
+        System.out.println("Hello test1");
+    }
+
+    @Test
+    public void test2() {
+        System.out.println("Hello test2");
+    }
+
+    @Test
+    public void test3() {
+        System.out.println("Hello test3");
+    }
+}
