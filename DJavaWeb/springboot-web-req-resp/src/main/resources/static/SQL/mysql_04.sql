e0e25f972cd4b1eff8b7aebb6367eed944e18806
ls <liushuo.fe@gmail.com>
2026-09-15 11:19:45 +0800

    feat: MySQL 事务 - 介绍&操作

--- DJavaWeb/springboot-web-req-resp/src/main/resources/static/SQL/mysql_04.sql
+++ DJavaWeb/springboot-web-req-resp/src/main/resources/static/SQL/mysql_04.sql
@@ -519,3 +519,28 @@ from dish
 where price < (select avg(price) from dish);
 
 
+-- ========================== 事务 ==========================
+-- 开启事务
+start transaction;
+
+-- 删除部门
+delete
+from tb_dept
+where id = 2;
+
+-- 删除部门下的员工
+delete
+from tb_emp
+where dept_id == 2;
+
+-- 提交事务
+commit;
+
+-- 回滚事务
+rollback;
+
+select *
+from tb_dept;
+
+select *
+from tb_emp;
